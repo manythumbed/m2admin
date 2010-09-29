d94e9e087dc20f775f1a7439062e8f51c2e05637
manythumbed <manythumbed@users.noreply.github.com>
2010-09-29 21:35:39 +0100

    More changes

--- src/m2admin/web.clj
+++ src/m2admin/web.clj
@@ -3,49 +3,22 @@
 	(:use [ring.middleware file])
 	(:use [ring.adapter.jetty])
 	(:use [net.cgrand.moustache])
-	(:use [net.cgrand.enlive-html]))
+	(:use [net.cgrand.enlive-html])
+	(:use [m2admin.table]))
 
-(def column-labels ["X" "Y" "Z" "P" "Q" "R"])
-
-(def column-head [:table.data :thead :tr :th]) 
-
-(def base-html (html-resource "templates/test.html"))
-(def header (select base-html [:thead first-child :th]))
-(def header-label 
-	(select base-html [[:thead :tr (nth-of-type 1)] :> first-child]))
-
-(defsnippet first-thead-row "templates/table.html"
-	[[:thead :tr (nth-of-type 1)]]
-	[value]
-	[:th] (content value))
-
-(defsnippet table-header-label "templates/base.html"
-	[[:tr (nth-of-type 1)] :> first-child]
-	[value]
-	[:th] (content value))
-
-(defsnippet cell-snippet "templates/test.html" 
-	[[:tr (nth-of-type 1)] :> first-child]
-	[value]
-	[:td] (do->
-					(content value)
-					(set-attr :id value)))
-
-(defsnippet fill-row "templates/test.html" [[:table :tbody (has [:tr])]]
-	[values]
-	[:tr] (content (map #(cell-snippet %) values))) 
-
-(defsnippet row-snippet "templates/base.html" [[:tr (has [:td])]]
-	[[v1 v2 v3]]
-	[[:td (nth-child 1)]] (content v1)
-	[[:td (nth-child 2)]] (content v2)
-	[[:td (nth-child 3)]] (content v3))
+(def primary-columns ["A" "B" "C"])
+(def secondary-columns ["a" "b" "c"])
+(def data 
+	[ ["1" "2" "3"]
+		["4" "5" "6"]
+		["7" "8" "9"] ])
 
 (deftemplate base "templates/base.html" [heading]
 	[:h1] (content heading)
-	[:table.data :thead :tr first-child] 
-		(clone-for [l column-labels] (content l))
-	[:table.data :tfoot :tr :th] (content "X"))
+	[:table] (content
+		(header-row primary-columns secondary-columns)
+		(table-data data)
+		(footer-row primary-columns secondary-columns)))
 
 (defn- render [t]
 	(apply str t))
